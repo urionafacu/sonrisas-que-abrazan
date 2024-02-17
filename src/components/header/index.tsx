83b8b46d18b6b35d04332285d5380f2df016b80e
Facundo Uriona <urionafacu@gmail.com>
2024-02-17 17:33:01 -0300

    fix: dynamic carousel height

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,7 +1,7 @@
 const Header = () => {
   return (
-    <header className='absolute top-0 left-0 right-0 z-10 flex justify-between items-center bg-black bg-opacity-70 h-40'>
-      <div className='bg-logo bg-cover w-28 h-28 ml-4' />
+    <header className='absolute top-0 left-0 right-0 z-10 flex justify-between items-center bg-black bg-opacity-70 h-52'>
+      <div className='bg-logo bg-cover w-36 h-36 ml-4' />
     </header>
   )
 }
