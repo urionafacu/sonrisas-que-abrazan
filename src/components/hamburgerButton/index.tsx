67e953b93485f6e25426352a2d5c13ff68934453
Facundo Uriona <urionafacu@gmail.com>
2024-06-25 19:16:04 -0300

    fix: bad behavior hamburger button

--- src/components/hamburgerButton/index.tsx
+++ src/components/hamburgerButton/index.tsx
@@ -1,17 +1,29 @@
 import s from './hamburgerButton.module.css'
 import { items } from '../header'
 import Link from 'next/link'
+import { useRef } from 'react'
 
 const HamburgerButton = () => {
+  const checkboxRef = useRef<HTMLInputElement>(null)
+  const handleLinkClick = () => {
+    if (checkboxRef.current) {
+      checkboxRef.current.checked = false
+    }
+  }
   return (
     <main className={s.container}>
       <label className={s.hamburgerMenu}>
-        <input type='checkbox' />
+        <input type='checkbox' ref={checkboxRef} />
       </label>
       <aside className={s.sidebar}>
         <nav className={s.nav}>
           {items.map((item) => (
-            <Link key={`${item.text}-hamburger-button`} href={item.href} className={s.link}>
+            <Link
+              key={`${item.text}-hamburger-button`}
+              href={item.href}
+              className={s.link}
+              onClick={handleLinkClick}
+            >
               {item.text}
             </Link>
           ))}
